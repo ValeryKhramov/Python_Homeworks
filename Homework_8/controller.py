0fcdadb617d4176f2adeb4a23deab86ccf720c2e
Valery_Khramov <dr.hramoff2013@yandex.ru>
2023-04-07 10:22:16 +0300

    Started working on the telephone directory

---
+++ Homework_8/controller.py
@@ -0,0 +1,36 @@
+import view
+import function_module as fun
+import text_module_ru as txt
+
+
+def start_phone_book():
+    while True:
+        choice = view.main_menu()
+        match choice:
+            case 1:
+                fun.load_file()
+                view.print_info(txt.load_successful)
+            case 2:
+                fun.save_file()
+                view.print_info(txt.save_successful)
+            case 3:
+                phone_book = fun.get_phone_book()
+                view.show_contact(phone_book, txt.no_contact_or_file)
+            case 4:
+                contact = view.new_contact()
+                fun.add_contact(contact)
+                view.print_info(txt.new_contact_successful)
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                if fun.exit_phone_book():
+                    if view.confirm(txt.is_changed):
+                        fun.save_file()
+                        view.print_info(txt.save_successful)
+                view.print_info(txt.bye_bye)
+                exit()
+
---
+++ Homework_8/view.py
@@ -0,0 +1,53 @@
+import text_module_ru as txt
+
+
+def main_menu() -> int:
+    print('''Главное меню:
+    1. Открыть файл
+    2. Сохранить файл
+    3. Показать все контакты
+    4. Создать контакт
+    5. Найти контакт
+    6. Изменить контакт
+    7. Удалить контакт
+    8. Выход''')
+    while True:
+        choice = input(txt.number_menu)
+        if choice.isdigit() and 0 < int(choice) < 9:
+            return int(choice)
+        else:
+            print(txt.menu_input_error)
+
+
+def show_contact(book: list[dict], message: str):
+    if book:
+        print('\n' + '-' * 63)
+        for n, contact in enumerate(book, 1):
+            print(f'{n:>3}. {contact.get("name"):<20}'
+                  f'{contact.get("phone"):<20}'
+                  f'{contact.get("comment"):<20}')
+        print('-' * 63 + '\n')
+    else:
+        print_info(txt.no_contact_or_file)
+
+
+def print_info(message: str):
+    print('\n' + '-' * len(message))
+    print(message)
+    print('-' * len(message) + '\n')
+
+
+def new_contact() -> dict:
+    name = input(txt.new_name)
+    phone = input(txt.new_phone)
+    comment = input(txt.new_comment)
+    return {'name': name, 'phone': phone, 'comment': comment}
+
+
+def confirm(message: str) -> bool:
+    print()
+    answer = input(message + '(y/n) : ')
+    if answer.lower() == 'y':
+        return True
+    else:
+        return False
